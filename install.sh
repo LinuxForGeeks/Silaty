b0d2398e58da881ca821119f082c94088c19dfde
mustafakhalaf <42213155+MustafaKhalaf69@users.noreply.github.com>
2022-07-24 16:37:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,8 +26,8 @@ cp icons/hicolor/24x24/apps/silaty.svg /usr/share/icons/hicolor/24x24/apps/
 chmod 644 /usr/share/icons/hicolor/24x24/apps/silaty.svg
 
 cp *.py /usr/share/silaty/
+cp *.png /usr/share/silaty/
 chmod 755 -R /usr/share/silaty/
-cp silaty.png /usr/share/silaty/
 
 cp silaty.desktop /etc/xdg/autostart/
 chmod 755 /etc/xdg/autostart/silaty.desktop
